c9a5ad75af36997a349288bda1036596688eb700
krysmaejaneCabillo <ckrysmaejane@gmail.com>
2026-05-21 10:09:19 +0800

    another changes

--- src/Modal/Register.vue
+++ src/Modal/Register.vue
@@ -147,7 +147,7 @@
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                     <button
-                        className="items-center text-center border border-gray-300 rounded-2xl py-3 font-medium hover:bg-gray-50 transition">
+                        className="items-center flex space-x-2 text-center border border-gray-300 rounded-2xl py-3 font-medium hover:bg-gray-50 transition">
                         <svg class="w-4 h-4 sm:w-5 sm:h-5 flex-shrink-0" viewBox="0 0 24 24" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
                             <path
@@ -163,18 +163,18 @@
                                 d="M12 5.38c1.57 0 2.98.54 4.09 1.6l3.09-3.09C17.46 2.14 14.97 1 12 1 7.7 1 3.99 2.47 2.18 5.38l3.66 2.84C6.72 5.92 9.13 5.38 12 5.38z"
                                 fill="#EA4335" />
                         </svg>
-                        Google
+                        <span>Google</span>
                     </button>
 
                     <button
-                        className="items-center text-center border border-gray-300 rounded-2xl py-3 font-medium hover:bg-gray-50 transition">
+                        className="items-center flex space-x-2 text-center border border-gray-300 rounded-2xl py-3 font-medium hover:bg-gray-50 transition">
                         <svg class="w-5 h-5 sm:w-6 sm:h-6 text-gray-800 flex-shrink-0" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
                             viewBox="0 0 24 24">
                             <path
                                 d="M17.537 12.625a4.421 4.421 0 0 0 2.684 4.047 10.96 10.96 0 0 1-1.384 2.845c-.834 1.218-1.7 2.432-3.062 2.457-1.34.025-1.77-.794-3.3-.794-1.531 0-2.01.769-3.275.82-1.316.049-2.317-1.318-3.158-2.532-1.72-2.484-3.032-7.017-1.27-10.077A4.9 4.9 0 0 1 8.91 6.884c1.292-.025 2.51.869 3.3.869.789 0 2.27-1.075 3.828-.917a4.67 4.67 0 0 1 3.66 1.984 4.524 4.524 0 0 0-2.16 3.805m-2.52-7.432A4.4 4.4 0 0 0 16.06 2a4.482 4.482 0 0 0-2.945 1.516 4.185 4.185 0 0 0-1.061 3.093 3.708 3.708 0 0 0 2.967-1.416Z" />
                         </svg>
-                        Apple
+                       <span>Apple</span>
                     </button>
                 </div>
 
